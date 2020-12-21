036906dd0d80f469751d2ea045aa509bd596d235
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-21 14:42:18 +0900

    implementing header buttons

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProductItem from '../../components/shop/ProductItem';
+import * as CartActions from '../../store/actions/cart';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
+    const dispatch = useDispatch();
     return (
         <FlatList
             data={products}
@@ -21,7 +23,9 @@ const ProductsOverviewScreen = props => {
                             productTitle: itemData.item.title
                         });
                     }}
-                    onAddToCart={() => { }}
+                    onAddToCart={() => {
+                        dispatch(CartActions.addToCart(itemData.item));
+                    }}
                 />
             )}
         />
