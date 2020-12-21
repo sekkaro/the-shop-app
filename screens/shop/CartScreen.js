55f3bcc1706815e025435c60a39ca84f7dcf02c3
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-21 16:39:08 +0900

    adding redux logic for orders

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import CartItem from '../../components/shop/CartItem';
 import Colors from '../../constants/Colors';
 import * as cartActions from '../../store/actions/cart';
+import * as ordersActions from '../../store/actions/orders';
 
 const CartScreen = props => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -35,6 +36,9 @@ const CartScreen = props => {
                     color={Colors.accent}
                     title="Order Now"
                     disabled={cartItems.length === 0}
+                    onPress={() => {
+                        dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
+                    }}
                 />
             </View>
             <FlatList
