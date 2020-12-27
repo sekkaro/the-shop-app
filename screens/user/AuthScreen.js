e8f851ceac40bd2d5e29495f8129e10e969f685a
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-28 08:09:01 +0900

    logging users in

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 import {
     ScrollView,
     StyleSheet,
@@ -40,6 +40,7 @@ const formReducer = (state, action) => {
 };
 
 const AuthScreen = props => {
+    const [isSignup, setIsSignup] = useState(false);
     const dispatch = useDispatch();
 
     const [formState, dispatchFormState] = useReducer(formReducer, {
@@ -54,13 +55,20 @@ const AuthScreen = props => {
         formIsValid: false
     });
 
-    const signupHandler = () => {
-        dispatch(
-            authActions.signup(
+    const authHandler = () => {
+        let action;
+        if (isSignup) {
+            action = authActions.signup(
                 formState.inputValues.email,
                 formState.inputValues.password
-            )
-        );
+            );
+        } else {
+            action = authActions.login(
+                formState.inputValues.email,
+                formState.inputValues.password
+            );
+        }
+        dispatch(action);
     };
 
     const inputChangeHandler = useCallback(
@@ -113,13 +121,18 @@ const AuthScreen = props => {
                         />
                         <View style={styles.buttonContainer}>
                             <Button
-                                title='Login'
+                                title={isSignup ? "Sign Up" : "Login"}
                                 color={Colors.primary}
-                                onPress={signupHandler}
+                                onPress={authHandler}
                             />
                         </View>
                         <View style={styles.buttonContainer}>
-                            <Button title='Switch to Sign Up' color={Colors.accent} onPress={() => { }} />
+                            <Button
+                                title={`Switch to ${isSignup ? 'Login' : 'Sign Up'}`}
+                                color={Colors.accent}
+                                onPress={() => {
+                                    setIsSignup(prevState => !prevState);
+                                }} />
                         </View>
                     </ScrollView>
                 </Card>
