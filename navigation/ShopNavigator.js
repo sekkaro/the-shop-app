39c9394f57a540b39a5917b2ff716feedb5f43dc
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2020-12-30 16:35:36 +0900

    adding logout

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { Platform } from 'react-native';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+import { Platform, SafeAreaView, Button, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { useDispatch } from 'react-redux';
+import * as authActions from '../store/actions/auth';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
@@ -86,6 +88,20 @@ const ShopNavigator = createDrawerNavigator({
 }, {
     contentOptions: {
         activeTintColor: Colors.primary
+    },
+    contentComponent: props => {
+        const dispatch = useDispatch();
+        return (
+            <View style={{ flex: 1, paddingTop: 20 }}>
+                <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+                    <DrawerItems {...props} />
+                    <Button title="Logout" color={Colors.primary} onPress={() => {
+                        dispatch(authActions.logout());
+                        props.navigation.navigate('Auth');
+                    }} />
+                </SafeAreaView>
+            </View>
+        );
     }
 });
 
